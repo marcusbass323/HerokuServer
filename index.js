f160b839db29600c84642d1631fad68e071da8c6
marcusbass323 <marcus@bassconsultantgroup.com>
2019-03-07 17:07:07 -0800

    Home server message up

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ server.use(cors())
 
 PORT = process.env.PORT || 7000;
 
-
+//GET / ENDPOINT
+server.get('/', (req, res) => {
+    console.log('Rendering Welcome Message')
+    res.send('Welcome to API')
+});
 
 server.listen(PORT, () => console.log(`running on port ${PORT}`));
