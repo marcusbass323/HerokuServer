117f89a4ee36a5f99d5756cd4ac8f3fbba133b10
marcusbass323 <marcus@bassconsultantgroup.com>
2019-03-09 21:22:47 -0800

    customer database up

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require('express')
+const express = require('express');
+const knex = require('knex');
+const dbConfig = require('./knexfile');
+const db = knex(dbConfig.development);
 const server = express();
+
+const bodyParser = require('body-parser');
+
+
 var cors = require('cors');
 
 server.use(express.json());
-server.use(cors())
+server.use(cors());
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 7000
 
 //GET / ENDPOINT
 server.get('/', (req, res) => {
